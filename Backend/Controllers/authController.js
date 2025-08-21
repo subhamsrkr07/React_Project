3a8ed537bb1caf09d86643d3d551dfefe3fd7ba2
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-22 04:38:06 +0530

    Update authController.js

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -23,8 +23,8 @@ export const signup=async(req,res)=>{
         let token = await gentoken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*1000
         })
         return res.status(201).json(user)
@@ -53,8 +53,8 @@ export const login=async(req,res)=>{
        let token = await gentoken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*1000
         })
         return res.status(201).json({message:"login succesfilly"}) 
